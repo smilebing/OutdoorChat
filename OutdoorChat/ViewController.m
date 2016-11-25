4073b503a1d5f49d80c8e544c1c09aa2298f62e8
smilebing <iblandshot@outlook.com>
2016-11-25 17:58:52 +0800

    创建项目

--- OutdoorChat/ViewController.m
+++ OutdoorChat/ViewController.m
@@ -5,20 +5,71 @@
 //  Created by 朱贺 on 2016/11/25.
 //  Copyright © 2016年 朱贺. All rights reserved.
 //
-
 #import "ViewController.h"
 
-@interface ViewController ()
 
+#import <XMPPFramework/XMPPFramework.h>
+
+@interface ViewController ()
+@property (nonatomic,strong) XMPPStream *stream ;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+-(XMPPStream *) InitStream
+{
+//    if(self.stream == nil )
+//    {
+        self.stream=[[XMPPStream alloc] init];
+        [self.stream addDelegate:self delegateQueue:dispatch_get_main_queue()];
+   // }
+    return self.stream;
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self InitStream];
+    [self.stream setMyJID:[XMPPJID jidWithString:@"zhuhe@127.0.0.1"]];
+    [self.stream setHostName:@"127.0.0.1"];
+    [self.stream setHostPort:5222];
+    
+    NSError *error=nil;
+    [self.stream connectWithTimeout:10.0 error:&error];
+    if(error)
+    {
+        NSLog(@"connectWithTimeout :%@",error);
+    }
+
+}
+
+-(void) xmppStreamDidConnect:(XMPPStream *)sender
+{
+    NSError * error=nil;
+    [self.stream authenticateWithPassword:@"zhuhe" error:&error];
+    NSLog(@"au");
+    if(error)
+    {
+        NSLog(@"authenticateWithPassword :%@",error);
+    }
+}
+
+-(void) xmppStreamDidAuthenticate:(XMPPStream *)sender
+{
+    XMPPPresence *presence=[XMPPPresence presence];
+    [self.stream sendElement:presence];
+}
+
+
+- (void)xmppStream:(XMPPStream *)sender didReceiveMessage:(XMPPMessage *)message
+{
+    NSString *msg = [[message elementsForName:@"body"] lastObject];
+    NSLog(@"%@",msg);
+}
+
+- (void)xmppStream:(XMPPStream *)sender didNotAuthenticate:(DDXMLElement *)error
+{
+    NSLog(@"didNotAuthenticate : %@",error);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'OutdoorChat' do
+use_frameworks!
+  pod 'AMap3DMap'  #3D地图SDK
+  #pod 'AMap2DMap' #2D地图SDK (2D和3D不能同时使⽤用)
+  pod 'AMapSearch' #搜索服务SDK
+
+pod 'XMPPFramework', :git => "https://github.com/robbiehanson/XMPPFramework.git", :branch => 'master'
+
+
+end

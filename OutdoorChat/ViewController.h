cb1fabccb10ad24b435373c971693dde1521dcf7
smilebing <iblandshot@outlook.com>
2016-11-25 13:33:58 +0800

    Initial Commit

---
+++ OutdoorChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OutdoorChat
+//
+//  Created by 朱贺 on 2016/11/25.
+//  Copyright © 2016年 朱贺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OutdoorChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OutdoorChat
+//
+//  Created by 朱贺 on 2016/11/25.
+//  Copyright © 2016年 朱贺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
